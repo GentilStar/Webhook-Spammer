2bdb937d649e03d4ed5b83636bfc980b50d00c99
GentilStar <86267632+GentilStar@users.noreply.github.com>
2022-04-11 23:49:16 -0300

    Add files via upload

---
+++ webhookspammer.py
@@ -0,0 +1,59 @@
+from colorama import Fore, Back, Style, init
+import os
+from dhooks import Webhook
+init()
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+    
+   
+cls()
+                                 
+print(Fore.MAGENTA +
+"""  
+
+                                    
+                                      ╔═╗┌─┐┬┬─┐╔╗ ┬  ┬ ┬┌─┐
+                                      ╔═╝├┤ │├┬┘╠╩╗""" + Fore.WHITE + """│  │ │├┤ 
+                                      ╚═╝└─┘┴┴└─╚═╝┴─┘└─┘└─┘ """ + Fore.WHITE + """
+                         =========="""+ Fore.GREEN + """ Discord Webhook-Spammer """ + Fore.WHITE + """==========""" + Fore.MAGENTA + """
+                                    ┌─────────────────────────┐   
+                                    │""" + Fore.WHITE + """ by GentilStar with """ + Fore.RED + """<3""" + Fore.MAGENTA + """   │                                
+                                    └─────────────────────────┘                                                             """ + Fore.RED + """   """)
+
+
+print('')
+i = input('Webhook: ')
+p = input('Message: ')
+print(Fore.GREEN + "")
+
+hook = Webhook(i)
+while True:
+ hook.send(p)
+ print(f"[+] {p}")
+
+ 
+
+
+cls()
+
+time.sleep(4)
+
+print(Fore.MAGENTA +
+"""  
+
+                                    
+                                      ╔═╗┌─┐┬┬─┐╔╗ ┬  ┬ ┬┌─┐
+                                      ╔═╝├┤ │├┬┘╠╩╗""" + Fore.WHITE + """│  │ │├┤ 
+                                      ╚═╝└─┘┴┴└─╚═╝┴─┘└─┘└─┘ """ + Fore.WHITE + """
+                         =========="""+ Fore.GREEN + """ Spamming... #LinkSquad """ + Fore.WHITE + """==========""" + Fore.MAGENTA + """
+                                    ┌─────────────────────────┐   
+                                    │""" + Fore.WHITE + """ by GentilStar with """ + Fore.RED + """<3""" + Fore.MAGENTA + """   │                                
+                                    └─────────────────────────┘                                                             """ + Fore.RED + """   """)
+                         
+
+time.sleep(1000)
+
+
+                         
+                         
